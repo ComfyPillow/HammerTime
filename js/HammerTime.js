52cc7cc460221d4f039c65cca7339a4a0a2e62a0
Nick Bohall <nabohall1224@icloud.com>
2015-11-28 19:33:09 -0800

    Made the basic example work

--- js/HammerTime.js
+++ js/HammerTime.js
@@ -1,4 +1,14 @@
-var hammertime = new Hammer(myElement, myOptions);
-hammertime.on('swipe', function() {
-    alert("test")
-});
+(function(){
+	window.onload = function() {
+		var myElement = document.getElementById('myElement');
+		console.log(myElement)
+		// create a simple instance
+		// by default, it only adds horizontal recognizers
+		var mc = new Hammer(myElement);
+
+		// listen to events...
+		mc.on("panleft panright tap press", function(ev) {
+		    myElement.textContent = ev.type +" gesture detected.";
+		});
+	}
+}());
