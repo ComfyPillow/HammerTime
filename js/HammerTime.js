7d0f05f9594eb1a5751765680578481563560ee5
George Ng <gngncp@uw.edu>
2015-12-01 09:56:55 -0800

    Added in Rotate

--- js/HammerTime.js
+++ js/HammerTime.js
@@ -11,42 +11,53 @@
 		// listen to events...
 		mc.on("panleft panright tap press", function(ev) {
 		    myElement.textContent = ev.type +" gesture detected.";
+		    mc.get('rotate').set({enable:true});
 		});
 
 		//Creates initial Circle - see if you can get it to add one each tap?
 		mc.on("tap", function(ev){
 
-			mySvg.append('circle')
-			    .attr('r', 50)
-			    .attr('cx', $(window).width()/2)
-			    .attr('cy', $(window).height()/3)
+			mySvg.append('rect')
+			    .attr('rx', 100)
+			    .attr('ry', 100)
+			    .attr('x', $(window).width()/2)
+			    .attr('y', $(window).height()/3)
+			    .attr('width', 100)
+			    .attr('height', 100)
 			    .attr('fill', '#add8e6')
 		});
 
 		//Slides Left
 		mc.on("panleft press", function(ev) {
-		    mySvg.selectAll('circle')
+		    mySvg.selectAll('rect')
     			.transition()
     			.duration(2000)
-  				.attr('cx', $(window).width()/4);
+  				.attr('x', $(window).width()/4);
 		});
 
 		//Slides Right
 		mc.on("panright press", function(ev) {
-		    mySvg.selectAll('circle')
+		    mySvg.selectAll('rect')
     			.transition()
     			.duration(2000)
-  				.attr('cx', ($(window).width()/4 * 3));
+  				.attr('x', ($(window).width()/4 * 3));
 		});
 
 		//Slides back to center
 		mc.on("press", function(ev){
-			mySvg.selectAll('circle')
+			mySvg.selectAll('rect')
 				.transition()
     			.duration(2000)
-    			.attr('cx', $(window).width()/2)
-			    .attr('cy', $(window).height()/3)
+    			.attr('x', $(window).width()/2)
+			    .attr('y', $(window).height()/3)
 		}); 
  
+ 		mc.on("rotate press", function(ev){
+			mySvg.selectAll('rect')
+				.transition()
+    			.duration(2000)
+    			.attr('rx', 0)
+			    .attr('ry', 0)
+		}); 
 	}
 }());
