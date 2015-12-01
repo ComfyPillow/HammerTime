cdbf9a15e39f8c4a855dcb0259261cdf14fdf1a8
George Ng <gngncp@uw.edu>
2015-11-30 18:41:58 -0800

    Fixed Bug

--- js/HammerTime.js
+++ js/HammerTime.js
@@ -7,19 +7,6 @@
 		var mc = new Hammer(myElement);
 		var mySvg = d3.select('#shape')
 
-
-		/*
-		function dragmove(d) {
-		    mySvg
-		      .style("top", ((d3.event.sourceEvent.pageY) - this.offsetHeight/2)+"px")
-		      .style("left", ((d3.event.sourceEvent.pageX) - this.offsetWidth/2)+"px")
-		}
-
-		var drag = d3.behavior.drag()
-	    	.on("drag", dragmove);
-		} */
-
-
 		// listen to events...
 		mc.on("panleft panright tap press", function(ev) {
 		    myElement.textContent = ev.type +" gesture detected.";
@@ -36,9 +23,20 @@
 			  .attr('fill', '#add8e6')
 		});
 
+		/*
 		mc.on("panleft press", function(ev) {
 		    mySvg.selectAll('circle')
     			.call(drag)
 		});
 
+
+		function dragmove(d) {
+		    
+		      .style("top", ((d3.event.sourceEvent.pageY) - this.offsetHeight/2)+"px")
+		      .style("left", ((d3.event.sourceEvent.pageX) - this.offsetWidth/2)+"px")
+		}
+
+		var drag = d3.behavior.drag()
+	    	.on("drag", dragmove); */
+	}
 }());
