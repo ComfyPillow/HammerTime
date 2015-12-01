9cfb3a2dbda7241ddff619f91109d1d984fbcf46
George Ng <gngncp@uw.edu>
2015-12-01 00:33:27 -0800

    Added animations in

--- js/HammerTime.js
+++ js/HammerTime.js
@@ -1,7 +1,7 @@
 (function(){
 	window.onload = function() {
 		var myElement = document.getElementById('myElement');
-		console.log(myElement)
+		$('#shape').css({'height':($(window).height()/3 * 2) + 'px','width':$(window).width() + 'px' });
 		// create a simple instance
 		// by default, it only adds horizontal recognizers
 		var mc = new Hammer(myElement);
@@ -12,31 +12,42 @@
 		    myElement.textContent = ev.type +" gesture detected.";
 		});
 
+		//Creates initial Circle - see if you can get it to add one each tap?
 		mc.on("tap", function(ev){
 
 			mySvg.selectAll('circle')
-			  .data([1])
-			  .enter().append('circle')
-			  .attr('r', 50)
-			  .attr('cx', $(window).width()/2)
-			  .attr('cy', $(window).height()/3)
-			  .attr('fill', '#add8e6')
+				.data([1])
+			    .enter().append('circle')
+			    .attr('r', 50)
+			    .attr('cx', $(window).width()/2)
+			    .attr('cy', $(window).height()/3)
+			    .attr('fill', '#add8e6')
 		});
 
-		/*
+		//Slides Left
 		mc.on("panleft press", function(ev) {
 		    mySvg.selectAll('circle')
-    			.call(drag)
+    			.transition()
+    			.duration(2000)
+  				.attr('cx', $(window).width()/4);
 		});
 
+		//Slides Right
+		mc.on("panright press", function(ev) {
+		    mySvg.selectAll('circle')
+    			.transition()
+    			.duration(2000)
+  				.attr('cx', ($(window).width()/4 * 3));
+		});
 
-		function dragmove(d) {
-		    
-		      .style("top", ((d3.event.sourceEvent.pageY) - this.offsetHeight/2)+"px")
-		      .style("left", ((d3.event.sourceEvent.pageX) - this.offsetWidth/2)+"px")
-		}
-
-		var drag = d3.behavior.drag()
-	    	.on("drag", dragmove); */
+		//Slides back to center
+		mc.on("press", function(ev){
+			mySvg.selectAll('circle')
+				.transition()
+    			.duration(2000)
+    			.attr('cx', $(window).width()/2)
+			    .attr('cy', $(window).height()/3)
+		}); 
+ 
 	}
 }());
