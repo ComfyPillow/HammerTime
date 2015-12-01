8e44fe64b0e474798b786c33f73c666a76d140f5
George Ng <gngncp@uw.edu>
2015-11-30 18:35:51 -0800

    Added in D3 Elements

--- js/HammerTime.js
+++ js/HammerTime.js
@@ -5,10 +5,40 @@
 		// create a simple instance
 		// by default, it only adds horizontal recognizers
 		var mc = new Hammer(myElement);
+		var mySvg = d3.select('#shape')
+
+
+		/*
+		function dragmove(d) {
+		    mySvg
+		      .style("top", ((d3.event.sourceEvent.pageY) - this.offsetHeight/2)+"px")
+		      .style("left", ((d3.event.sourceEvent.pageX) - this.offsetWidth/2)+"px")
+		}
+
+		var drag = d3.behavior.drag()
+	    	.on("drag", dragmove);
+		} */
+
 
 		// listen to events...
 		mc.on("panleft panright tap press", function(ev) {
 		    myElement.textContent = ev.type +" gesture detected.";
 		});
-	}
+
+		mc.on("tap", function(ev){
+
+			mySvg.selectAll('circle')
+			  .data([1])
+			  .enter().append('circle')
+			  .attr('r', 50)
+			  .attr('cx', $(window).width()/2)
+			  .attr('cy', $(window).height()/3)
+			  .attr('fill', '#add8e6')
+		});
+
+		mc.on("panleft press", function(ev) {
+		    mySvg.selectAll('circle')
+    			.call(drag)
+		});
+
 }());
