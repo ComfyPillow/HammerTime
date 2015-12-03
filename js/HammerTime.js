deccb4efcaf4dcb6c5f35f7ccd00543c0dbfe4da
George Ng <gngncp@uw.edu>
2015-12-02 17:57:38 -0800

    trying to fix pinch

--- js/HammerTime.js
+++ js/HammerTime.js
@@ -94,7 +94,7 @@
 		//Rotate fills and changes to a rectangle
 		mc.on("pinch", function(ev){
 			var colorScale = d3.scale.category10();
-			var numbers = d3.range(1, mySvg.selectAll('rect').size() + 1);
+			var numbers = d3.range(1, mySvg.selectAll('rect').size());
 
 			mySvg.selectAll('rect')
 				.data(numbers)
